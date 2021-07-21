be03813e7fb52dc24639b74e38fe8b8bea2e038c
izacximenes <izac98ximenes@gmail.com>
2021-07-21 10:37:56 -0300

    improve func create file

--- main.go
+++ main.go
@@ -48,38 +48,32 @@ func fileExists(name string) bool {
 	return true
 }
 
-func makeDirectoryAndFileIfNotExists(path string, file string, template string) error {
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-
-		err := os.Mkdir(path, os.ModeDir|0755)
-
-		if err != nil {
-			panic(err.Error())
-		}
-
+func mkdirAll(p string, template string) (*os.File, error) {
+	if err := os.MkdirAll(filepath.Dir(p), 0770); err != nil {
+		return nil, err
 	}
 
-	pathFull := filepath.Join(path, filepath.Base(file))
-	if fileExists(pathFull) {
+	if fileExists(filepath.Join(p)) {
 		log.Fatalln("File exist")
 	}
-	fileCreated, err := os.Create(pathFull)
 
-	if len(template) > 0 {
-		fileTemplate, err := ioutil.ReadFile(template)
-		if err != nil {
-			log.Fatalln(err.Error())
-		}
-		fileCreated.Write(fileTemplate)
-
-	}
+	fileCreated, err := os.Create(p)
 
 	if err != nil {
 		panic(err.Error())
 	}
-	fmt.Println("File created ", pathFull)
 
-	return nil
+	if len(template) > 0 {
+		fileTemplate, err := ioutil.ReadFile(filepath.Join(template))
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+		fileCreated.Write(fileTemplate)
+	}
+
+	fmt.Println("File created ", p)
+
+	return fileCreated, nil
 }
 
 func main() {
@@ -89,7 +83,7 @@ func main() {
 		panic(err)
 	}
 	rootCmd.ResetCommands()
-	for key, _ := range yaml.Modules {
+	for key := range yaml.Modules {
 		vars := yaml.Modules[key]
 		var command = key
 
@@ -108,7 +102,8 @@ func main() {
 			Run: func(cmd *cobra.Command, args []string) {
 				filename := args[0]
 				file := fmt.Sprintf("%s%s%s.%s", vars.Prefix, filename, vars.Suffix, yaml.Project.Extension)
-				err = makeDirectoryAndFileIfNotExists(vars.Path, file, vars.Template)
+				// err = makeDirectoryAndFileIfNotExists(vars.Path, file, vars.Template)
+				mkdirAll(fmt.Sprintf("%s%s", vars.Path, file), vars.Template)
 				if err != nil {
 					panic(err.Error())
 				}
@@ -117,6 +112,14 @@ func main() {
 		}
 		rootCmd.AddCommand(cmd)
 	}
-
+	rootCmd.AddCommand(versionCmd)
 	rootCmd.Execute()
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of myCLI",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("myCLI v0.1")
+	},
 }
